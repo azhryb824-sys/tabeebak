0e64258960679ea3117127faa56ab5f649f1277a
azhryb824-sys <azhryb824@gmail.com>
2026-04-09 02:47:21 +0300

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-application = get_wsgi_application()
+import appointments.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            appointments.routing.websocket_urlpatterns
+        )
+    ),
+})
