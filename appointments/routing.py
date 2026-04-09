d6c1a2ddbe370fb2676aa0fc589932be65cb9c37
azhryb824-sys <azhryb824@gmail.com>
2026-04-09 03:18:41 +0300

    Update routing.py

--- appointments/routing.py
+++ appointments/routing.py
@@ -2,5 +2,8 @@ from django.urls import re_path
 from .consumers import AppointmentChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/appointments/(?P<appointment_id>\d+)/chat/$", AppointmentChatConsumer.as_asgi()),
+    re_path(
+        r"ws/appointments/(?P<appointment_id>\d+)/chat/$",
+        AppointmentChatConsumer.as_asgi()
+    ),
 ]
